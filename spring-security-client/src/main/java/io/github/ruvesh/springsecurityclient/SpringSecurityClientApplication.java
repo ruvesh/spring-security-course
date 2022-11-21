62d9e27170d9f6d1e802c0d4efbdbc0b50428111
ruvesh <ruvesh2145@gmail.com>
2022-11-21 15:15:31 +0530

    initialize spring security parent project and client module

---
+++ spring-security-client/src/main/java/io/github/ruvesh/springsecurityclient/SpringSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package io.github.ruvesh.springsecurityclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClientApplication.class, args);
+	}
+
+}
